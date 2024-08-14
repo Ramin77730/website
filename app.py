81cd4c950b28d50cade8beb42347080cb1cee68c
RaminRodionov132 <Project@toosrs.kz>
2024-08-14 14:13:32 +0500

    14.08.24/14.12

--- app.py
+++ app.py
@@ -25,9 +25,51 @@ def index():
     return render_template("Index.html")
 
 
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article = Article.query.get(id)
+    return render_template("posts_detail.html", article=article)
+
+
+@app.route('/posts/<int:id>/del')
+def posts_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении статьи возникла ошибка"
+
+
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При редактировании статьи произощла ошибка"
+    else:
+
+        return render_template("post_update.html", article=article)
 
 
 @app.route('/create-article', methods=['POST', 'GET'])
@@ -42,7 +84,7 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return "При добавлении статьи произощла ошибка"
     else:
